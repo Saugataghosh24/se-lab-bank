3efd726d3e7e0c8ffc989454edd363e6432ab42c
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-06-09 15:17:38 +0530

    last commit

--- Main.java
+++ Main.java
@@ -1,37 +1,40 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws FileNotFoundException, IOException {
         Bank bank = new Bank();
-        try (InputStreamReader isr = new InputStreamReader(System.in);
-                BufferedReader br = new BufferedReader(isr);) {
-            System.out.println("-------- Welcome to the Banking System --------");
-            System.out.print("Enter Account Number :: ");
-            int accountNumber = Integer.parseInt(br.readLine());
-            boolean continueTransaction = true;
-            while (continueTransaction) {
-                System.out.println("Enter 0 to add balance, 1 to withdraw balance, -1 to exit: ");
-                int choice = Integer.parseInt(br.readLine());
-                switch (choice) {
-                    case 0:
-                        bank.addAmount(accountNumber);
-                    case 1:
-                        bank.withdraw(accountNumber);
-                    default:
-                        continueTransaction = false;
-                        break;
-                }
-            }
-            bank.write();
-        } catch (FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------- Welcome to the Banking System --------");
 
+        System.out.print("Enter Account Number :: ");
+        int accountNumber = sc.nextInt();
+        sc.nextLine();
+        if (!bank.checkAccountExistance(accountNumber)) {
+            System.out.println("Account does not exists!");
+            System.exit(1);
+        }
+        boolean continueTransaction = true;
+        while (continueTransaction) {
+            System.out.print("Enter 0 to add balance, 1 to withdraw, -1 to exit :: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 0:
+                    bank.addAmount(accountNumber);
+                    break;
+                case 1:
+                    bank.withdraw(accountNumber);
+                    break;
+                default:
+                    continueTransaction = false;
+                    break;
+            }
+        }
+        sc.close();
+        bank.write();
     }
 }
