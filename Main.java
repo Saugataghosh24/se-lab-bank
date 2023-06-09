06784583dddf73642f8c37987266cd9eefafe60f
Md Atif <www.seikhatif@gmail.com>
2023-06-09 15:07:52 +0530

    hashmap included

--- Main.java
+++ Main.java
@@ -7,12 +7,15 @@ import java.util.HashMap;
 public class Main {
     public static void main(final String[] args) {
         Bank bank = new Bank();
+        HashMap<Integer, AccountDetails>mp=bank.getMap();
         try (InputStreamReader isr = new InputStreamReader(System.in);
                 BufferedReader br = new BufferedReader(isr);) {
             System.out.println("-------- Welcome to the Banking System --------");
             System.out.print("Enter Account Number :: ");
             int accountNumber = Integer.parseInt(br.readLine());
             boolean continueTransaction = true;
+            if(mp.containsKey(accountNumber))
+            {
             while (continueTransaction) {
                 System.out.println("Enter 0 to add balance, 1 to withdraw balance, -1 to exit: ");
                 int choice = Integer.parseInt(br.readLine());
@@ -27,6 +30,9 @@ public class Main {
                 }
             }
             bank.write();
+        }
+        else
+        System.out.println("Account number does not exits in our Bank");
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         } catch (IOException ioe) {
